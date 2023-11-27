0b432a28904c78ba00ab55905d24b2f4df507019
MD AMRAN MONDOL <imranbsmrstucse@gmail.com>
2023-11-27 14:03:55 +0600

    AddEntity

--- Digital.Diary/Digital.Diary.Databases/Data/ApplicationDbContext.cs
+++ Digital.Diary/Digital.Diary.Databases/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace Digital.Diary.Databases.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext:DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
     }
 }
---
+++ Digital.Diary/Digital.Diary.Models/EntityModels/Administrative/Administration/Director.cs
@@ -0,0 +1,14 @@
+﻿using Digital.Diary.Models.BaseEntityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital.Diary.Models.EntityModels.Administrative.Administration
+{
+    public class Director:BaseEntity
+    {
+
+    }
+}
